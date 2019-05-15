083e72b3b294ff80ea4eefb2c6ba48c4c4805550
ailuoy <3211028@qq.com>
2019-05-16 00:05:08 +0800

    feat:(api): 增加 chatRoom 接口

--- src/Factory/ChatRoomModelFactory.php
+++ src/Factory/ChatRoomModelFactory.php
@@ -11,13 +11,21 @@ namespace Ailuoy\NeteaseIm\Factory;
 use Ailuoy\NeteaseIm\Models\ChatRoom\Create;
 use Ailuoy\NeteaseIm\Models\ChatRoom\Get;
 use Ailuoy\NeteaseIm\Models\ChatRoom\GetBatch;
+use Ailuoy\NeteaseIm\Models\ChatRoom\RequestAddr;
+use Ailuoy\NeteaseIm\Models\ChatRoom\SetMemberRole;
+use Ailuoy\NeteaseIm\Models\ChatRoom\ToggleCloseStat;
+use Ailuoy\NeteaseIm\Models\ChatRoom\Update;
 use Ailuoy\NeteaseIm\ResultReturn;
 use Ailuoy\NeteaseIm\Exceptions\ResultReturnException;
 
 /**
- * @property Create   create
- * @property Get      get
- * @property GetBatch getBatch
+ * @property Create          create
+ * @property Get             get
+ * @property GetBatch        getBatch
+ * @property Update          update
+ * @property ToggleCloseStat toggleCloseStat
+ * @property SetMemberRole   setMemberRole
+ * @property RequestAddr     requestAddr
  *
  *
  * Class ModuleFactory
@@ -25,13 +33,21 @@ use Ailuoy\NeteaseIm\Exceptions\ResultReturnException;
  */
 class ChatRoomModelFactory
 {
-
+    /**
+     * 模型列表
+     *
+     * @return \Ailuoy\NeteaseIm\ResultReturnStructure
+     */
     private function modelList()
     {
         $modelList = [
-            'create'   => Create::class,
-            'get'      => Get::class,
-            'getBatch' => GetBatch::class
+            'create'          => Create::class,
+            'get'             => Get::class,
+            'getBatch'        => GetBatch::class,
+            'update'          => Update::class,
+            'toggleCloseStat' => ToggleCloseStat::class,
+            'setMemberRole'   => SetMemberRole::class,
+            'requestAddr'     => RequestAddr::class,
         ];
 
         return ResultReturn::success($modelList);
@@ -46,6 +62,12 @@ class ChatRoomModelFactory
     {
     }
 
+    /**
+     * @param $name
+     *
+     * @return mixed
+     * @throws ResultReturnException
+     */
     public function __get($name)
     {
         $modelList = $this->modelList()->data;
--- src/Models/Model.php
+++ src/Models/Model.php
@@ -17,16 +17,44 @@ abstract class Model implements ModelInterface
 {
     protected $args = [];
 
+    /**
+     * 合并参数
+     *
+     * @param array $arr
+     *
+     * @return array
+     */
     protected function mergeArgs(array $arr)
     {
         return array_merge($arr, $this->args);
     }
 
+    /**
+     * 参数校验
+     *
+     * @param array $rules
+     * @param array $messages
+     * @param array $parameters
+     *
+     * @throws \Ailuoy\NeteaseIm\Exceptions\ParameterErrorException
+     */
     protected function validation(array $rules, array $messages, array $parameters)
     {
         ValidationFactory::check($rules, $messages, $parameters);
     }
 
+    /**
+     * 发送http请求
+     *
+     * @param $method
+     * @param $api
+     * @param $parameters
+     *
+     * @return mixed
+     * @throws \Ailuoy\NeteaseIm\Exceptions\ParameterErrorException
+     * @throws \Ailuoy\NeteaseIm\Exceptions\RequestErrorException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     protected function sendRequest($method, $api, $parameters)
     {
         return HttpClient::send($method, $api, $parameters)->data;
